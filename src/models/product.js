255b7125f16c4d9b20ad3d32c842dbbee26b11f6
trungtd1 <107098421+trungtd1@users.noreply.github.com>
2022-07-29 15:27:26 +0700

    #23 commit model Product

--- src/models/product.js
+++ src/models/product.js
@@ -1,46 +1,46 @@
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 
-// const productSchema = mongoose.Schema({
-//     name: {
-//         type: String,
-//         required: true
-//     },
-//     price: {
-//         type: Number,
-//         required: true
-//     },
-//     Image: {
-//         type: String,
-//         required: true
-//     },
-//     sale: {
-//         type: Number,
-//         required: true
-//     },
-//     longDesc: {
-//         type: String,
-//         required: true
-//     },
-//     shortDesc: {
-//         type: String,
-//         required: true
-//     },
-//     salientfeatures: {
-//         type: String,
-//         required: true
-//     },
-//     category: {
-//         type: Number,
-//         required: true
-//     },
-//     id: {
-//         type: Number,
-//         required: true
-//     },
-//     isDelete: {
-//         type: Boolean,
-//         required: true
-//     }
+const productSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    Image: {
+        type: String,
+        required: true
+    },
+    sale: {
+        type: Number,
+        required: true
+    },
+    longDesc: {
+        type: String,
+        required: true
+    },
+    shortDesc: {
+        type: String,
+        required: true
+    },
+    salientfeatures: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: Number,
+        required: true
+    },
+    id: {
+        type: Number,
+        required: true
+    },
+    isDelete: {
+        type: Boolean,
+        required: true
+    }
 
-// });
-// export default mongoose.model('Product', productSchema)
+});
+export default mongoose.model('Product', productSchema)
