737ba5c128a6bd6bd377e54014fab3e82ae26006
trungtd1 <107098421+trungtd1@users.noreply.github.com>
2022-07-29 15:30:14 +0700

    #33 commit routes product

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,13 +1,13 @@
-// import express from 'express';
-// import { add, list, read, remove, update } from '../controllers/product';
+import express from 'express';
+import { add, list, read, remove, update } from '../controllers/product';
 
-// const router = express.Router();
+const router = express.Router();
 
 
-// router.get("/products", list);
-// router.get("/products/:id", read);
-// router.post("/products", add);
-// router.delete("/products/:id", remove);
-// router.patch("/products/:id", update);
+router.get("/products", list);
+router.get("/products/:id", read);
+router.post("/products", add);
+router.delete("/products/:id", remove);
+router.patch("/products/:id", update);
 
-// export default router;
+export default router;
