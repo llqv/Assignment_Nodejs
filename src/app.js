c1e4d6a8b7b411f166212541bedbfc0d810d2ebb
ninhadmit147 <59793130+ninhadmit147@users.noreply.github.com>
2022-07-29 15:13:44 +0700

    #27 ConnetDb successfully

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/Asm_nodeJs", () => {
     console.log("Connected");
 })
 
+
 app.listen(process.env.PORT, () => {
     console.log("Kết nối thành công cổng: " + process.env.PORT);
 })
