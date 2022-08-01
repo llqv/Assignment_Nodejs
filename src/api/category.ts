dce4ec396584c0385eb4ea5502f1e0fab6866cc2
Duongld <duongldph15278@fpt.edu.vn>
2022-08-01 15:54:33 +0700

    #49 create api category

--- src/api/category.ts
+++ src/api/category.ts
@@ -5,7 +5,7 @@ export const listCategory = () => {
     return instance.get('/category')
 }
 
-// Category theo id
+// Category by id
 export const catebyId = (id: any) => {
     return instance.get(`/category/${id}`)
 }
