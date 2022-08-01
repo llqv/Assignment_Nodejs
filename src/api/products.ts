b0be0a81741d963298b938e46ae538c3da8bbd4c
ninhadmit147 <59793130+ninhadmit147@users.noreply.github.com>
2022-08-01 15:19:39 +0700

    #47 commit API file config & product

---
+++ src/api/products.ts
@@ -0,0 +1,23 @@
+import Product from "../models/product"
+import instance from "./config"
+
+//list
+export const List = () => {
+    return instance.get(`/products`)
+}
+//read
+export const Read = (id: any) => {
+    return instance.get(`/products/${id}`)
+}
+//add
+export const Create = (product: Product[]) => {
+    return instance.get(`/products`, product)
+}
+//update
+export const Update = (product: Product[]) => {
+    return instance.get(`/products/${product.id}`, product)
+}
+//List by cate
+export const Listbycate = (category: number) => {
+    return instance.get(`/products?category=${category}`)
+}
