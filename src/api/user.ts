6c40b12bebf5c500f8a2369236f1ff005cad1df5
llqv <lequangvu070501@gmail.com>
2022-08-04 22:23:29 +0700

    #62 fix

--- src/api/user.ts
+++ src/api/user.ts
@@ -10,3 +10,8 @@ export const login = (user: User[]) => {
     const url = "/login"
     return instance.post(url, user)
 }
+
+export const infoUser = (token: string) => {
+    const url = `/infoUser/${token}`
+    return instance.get(url)
+}
