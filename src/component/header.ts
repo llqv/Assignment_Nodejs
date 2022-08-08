f4d73f41aca37c35d0dc20a8f669a00f2b3921ac
llqv <lequangvu070501@gmail.com>
2022-08-08 12:31:52 +0700

    #68 fix : header home

--- src/component/header.ts
+++ src/component/header.ts
@@ -43,23 +43,6 @@ const Header = {
                   Giỏ hàng
                 </a>
                 ${
-<<<<<<< HEAD
-                       avatar?`
-                       <img class="rounded-full border-2 bg-white h-12 w-12 my-1 mx-1 mt-2" src=${avatar} />
-                       <button onclick="localStorage.removeItem('token');
-                       window.location.href='/'; alert('Đăng xuất thành công')">
-                       <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 mt-1 mx-1" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                       <path stroke-linecap="round" stroke-linejoin="round" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
-                       </svg>
-                       </button>
-                       `:`
-                       <a class="flex w-[100px] my-2 " href="/signin">
-                       <svg xmlns="http://www.w3.org/2000/svg"  class="h-10 w-10 my-1 mx-1" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                         <path stroke-linecap="round" stroke-linejoin="round" d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1" />
-                       </svg>
-                     </a>`
-                }
-=======
                   avatar?`
                     <img class="rounded-full border-2 bg-white h-12 w-12 my-1 mx-1 mt-2" src=${avatar} />
                     <button onclick="localStorage.removeItem('token');
